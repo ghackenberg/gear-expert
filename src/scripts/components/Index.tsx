ba6f74495ace1e87d84f20a9e15e0c9064c5c063
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2024-07-15 14:20:28 +0200

    start working on interaction

--- src/scripts/components/Index.tsx
+++ src/scripts/components/Index.tsx
@@ -26,14 +26,23 @@ export function Index() {
     const speed2 = -speed1 * teeth1 / teeth2
     const speed3 = -speed2 * teeth2 / teeth3
 
-    const gears: GearProps[] = [
+    const initialGears: GearProps[] = [
         { teeth: teeth1, style: 'plain', position: [x1, 0, 0], angle: angle1, speed: speed1 },
         { teeth: teeth2, style: 'plain', position: [x2, 0, 0], angle: angle2, speed: speed2 },
         { teeth: teeth3, style: 'plain', position: [x3, 0, 0], angle: angle3, speed: speed3 }
     ]
+
+    const [gears, setGears] = React.useState(initialGears)
+
+    function onPointerMissed(event: MouseEvent) {
+        const x = (event.pageX / window.innerWidth - 0.5) * 2
+        const y = (event.pageY / window.innerHeight - 0.5) * 2
+        
+        setGears([...gears, { teeth: 10, style: 'plain', position: [x / 10, 0, y / 20], angle: 0, speed: 0 }])
+    }
     
     return (
-        <Canvas orthographic camera={{ zoom: 5000, position: [0, 1, 0] }}>
+        <Canvas orthographic camera={{ zoom: 5000, position: [0, 1, 0] }} onPointerMissed={onPointerMissed}>
             { /*Light*/ }
             <ambientLight intensity={Math.PI / 2} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
